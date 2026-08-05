3c2699885d9a17aa87a2e7819c676f64ed576bab
Deivid-20 <deivid.collaguazo2006@gmail.com>
2026-08-05 11:48:31 -0500

    David: Avance inicial de validaciones en Semaforo.js

--- Funciones/Semaforo.js
+++ Funciones/Semaforo.js
@@ -1,2 +1,13 @@
+/**
+ * Área de David: Lógica del Semáforo Académico
+ * En proceso de desarrollo...
+ */
+export function evaluarSemaforo(nota) {
+    // Implementacion de validaciones y rangos
+    if (isNaN(nota) || nota < 0 || nota > 10) {
+        return { estado: "No válido", color: "gris", mensaje: "Error en el rango" };
+    }
+    return { estado: "Pendiente", color: "gris", mensaje: "" };
+}
 
 
